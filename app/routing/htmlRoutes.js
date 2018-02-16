3a32efff9c71d00c9070cd1ba0198606f2b1b0fe
Barry Williams <barrywilliams84@gmail.com>
2018-02-15 22:21:12 -0800

    removed uncessary code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,24 +1,15 @@
 
-// Your `htmlRoutes.js` file should include two routes:
-
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default, catch-all route that leads to `home.html` which displays the home page. 
-
 
 var path = require("path");
 
 ///////////////////////////////////////
-// Routing
+// Routing for html files
 //////////////////////////////////////
 
 module.exports = function(app) {
 	// route for survey 
 	app.get("/survey", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
-	});
-	// route for home 
-	app.get("/home", function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 	// if route doesnt match then default to home
 	app.get("*", function(req, res) {
