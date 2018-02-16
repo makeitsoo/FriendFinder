11aa2604e7426ba99c6ed3f511035df0632fe244
Barry Williams <barrywilliams84@gmail.com>
2018-02-15 22:10:46 -0800

    server.js code complete, and html routes complete

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,3 +3,26 @@
 
 //    * A GET Route to `/survey` which should display the survey page.
 //    * A default, catch-all route that leads to `home.html` which displays the home page. 
+
+
+var path = require("path");
+
+///////////////////////////////////////
+// Routing
+//////////////////////////////////////
+
+module.exports = function(app) {
+	// route for survey 
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
+	});
+	// route for home 
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
+	// if route doesnt match then default to home
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
+};
+
