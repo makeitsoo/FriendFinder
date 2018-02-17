d16aa6a48c99fa76e00339010d15cee3e85de3cf
Barry Williams <barrywilliams84@gmail.com>
2018-02-17 11:01:18 -0800

    friend matching logic in apiRoutes complete - now displays best match in modal on DOM

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,3 +18,39 @@
 //     ]
 // }
 // ```
+var friendsData = [
+
+	{
+		name: 'Ron Burgandy',
+		photo: 'https://pbs.twimg.com/profile_images/1990249248/image.jpg',
+		scores: [2, 5, 1, 3, 2, 4, 1, 2, 1, 1]
+
+	}, {
+		name: 'Rick Grimes',
+		photo: 'https://undeadwalking.com/wp-content/blogs.dir/318/files/2014/12/tumblr_nfvsj4l1vV1qzb6bqo4_1280rick1.jpg',
+		scores: [2, 2, 1, 3, 2, 4, 5, 5, 1, 1]
+
+	}, {
+		name: 'Michael Scott',
+		photo: 'http://www.etonline.com/sites/default/files/images/2017-01/1280_Steve_Carell_Michael-Scott_TheOffice_NBC.jpg',
+		scores: [1, 2, 1, 5, 1, 4, 4, 1, 3, 3]
+
+	}, {
+		name: 'Zombie',
+		photo: 'http://ksassets.timeincuk.net/wp/uploads/sites/54/2016/02/zombies-3.jpg',
+		scores: [2, 4, 4, 1, 2, 5, 5, 1, 2, 1]
+	}, {
+		name: 'Leloo Dallas Multipass',
+		photo: 'https://i.imgur.com/muvswfA.jpg',
+		scores: [5, 1, 2, 2, 1, 1, 5, 5, 5, 1]
+	}, {
+		name: 'Hellraiser',
+		photo: 'http://cdn.bloody-disgusting.com/wp-content/uploads/2016/08/Hellraiser.jpeg',
+		scores: [3, 4, 1, 5, 1, 3, 2, 2, 1, 1]
+	}
+
+
+
+];
+
+module.exports = friendsData;
